3bede5c5fc5e71848a57f33a94faf51fc8b6dd55
Theodore Xavier <theodore.xavier221@gmail.com>
2023-06-01 15:03:32 +1000

    Initial Commit

---
+++ src/components/nav/Nav.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./nav.css";
+import { AiOutlineHome } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { BsBookHalf } from "react-icons/bs";
+import { AiFillPhone } from "react-icons/ai";
+import { useState } from "react";
+
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState("#");
+  return (
+    <nav>
+      <a
+        href="#"
+        onClick={() => setActiveNav("#")}
+        className={activeNav === "#" ? "active" : ""}
+      >
+        <AiOutlineHome />
+      </a>
+      <a
+        href="#about"
+        onClick={() => setActiveNav("#about")}
+        className={activeNav === "#about" ? "active" : ""}
+      >
+        <AiOutlineUser />
+      </a>
+      <a
+        href="#experience"
+        onClick={() => setActiveNav("#experience")}
+        className={activeNav === "#experience" ? "active" : ""}
+      >
+        <BsBookHalf />
+      </a>
+      <a
+        href="#contact"
+        onClick={() => setActiveNav("#contact")}
+        className={activeNav === "#contact" ? "active" : ""}
+      >
+        <AiFillPhone />
+      </a>
+    </nav>
+  );
+};
+
+export default Nav;
